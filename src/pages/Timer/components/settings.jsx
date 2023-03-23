093d4596f093e0811c1714ec5c679e18b25fc82d
Jes015 <>
2023-03-23 02:39:57 +0000

    feat(volume): add volume setting for timer

---
+++ src/pages/Timer/components/settings.jsx
@@ -0,0 +1,13 @@
+// Custom Components
+import { InputButton, Slider } from '../../../components/'
+
+export default function Settings ({ setAlarmTone, setAlarmVolume, alarmVolume }) {
+  return (
+    <>
+      <div>
+        <InputButton setData={setAlarmTone} placeholder='URL' id='URL' label='Song' />
+        <Slider setData={setAlarmVolume} value={alarmVolume} id='volume' label='Volume' />
+      </div>
+    </>
+  )
+}
